13d7efac148abf7b69633ef2783ffee67c50f2f3
mierzvoj <mierzvoj@gmail.com>
2022-11-03 22:26:40 +0100

    fourth commit, final version

--- main.py
+++ main.py
@@ -4,74 +4,65 @@ import skfuzzy.membership as mf
 import matplotlib.pyplot as plt
 
 # Input values
-input_passengers = int(input("Input number of passengers: "))
+driving_style = int(input("Input your av. energy consumption in Wh/km: "))
 input_temperature = int(input("Input temperature: "))
 input_battery_capacity = int(input("Input battery size: "))
 
-x_range = np.arange(0, 380, 1)
-x_temperature = np.arange(-30, 50, 1)
-
-# Tesla model 4 batteries types
-# Batteries in kWh
-batt_capacity_low = int(54)
-batt_capacity_medium = int(62)
-batt_capacity_medium_higher = int(75)
-batt_capacity_high = int(82)
+x_range = np.arange(0, 640, 1)
+x_temperature = np.arange(-10, 23, 1)
+# Power in kWh of estimated remaining battery capacity per 100 km
+x_estimation = np.arange(10, 24, 1)
+x_battery = np.arange(16, 108, 1)
 # Membership functions
+# Weather
+weather_cold = mf.trapmf(x_temperature, [-10, -5, 0, 1])
+weather_mild = mf.trapmf(x_temperature, [0, 1, 23, 23])
 # Batteries
-range_long_min = mf.trapmf(x_range, [0, 0, 320, 328])
-range_long_med = mf.trapmf(x_range, [0, 0, 330, 338])
-range_long_med_high = mf.trapmf(x_range, [0, 0, 340, 348])
-range_long_max = mf.trapmf(x_range, [0, 0, 350, 358])
-# Ranges in route mode
-range_city = mf.trapmf(x_range, [0, 0, 250, 320])
-range_highway = mf.trapmf(x_range, [0, 0, 350, 358])
-# Ranges vs temp
-range_temp_lower_than_min10 = mf.trapmf(x_range, [0, 200, 200, 200])
-range_temp_min10_0 = mf.trapmf(x_range, [0, 250, 250, 250])
-range_temp_0_10 = mf.trapmf(x_range, [0, 350, 350, 350])
-# Range combined with passengers number
-range_passengers_two = mf.trapmf(x_range, [0, 350, 358, 358])
-range_passengers_four = mf.trapmf(x_range, [0, 350, 350, 350])
-# Ranges low, mid, long, output membership function
-range_low = mf.trapmf(x_range, [100, 150, 200, 250])
-range_mid = mf.trapmf(x_range, [200, 250, 300, 350])
-range_long = mf.trapmf(x_range, [300, 350, 380, 400])
-
+batt_low = mf.trapmf(x_battery, [15, 16, 20, 25])
+batt_medium = mf.trapmf(x_battery, [20, 25, 60, 65])
+batt_medium_high = mf.trapmf(x_battery, [60, 65, 90, 95])
+batt_high = mf.trapmf(x_battery, [90, 95, 108, 113])
+# Driving modes in Wh/km
+city_drive = mf.trapmf(x_estimation, [100, 110, 120, 130])
+combined_drive = mf.trapmf(x_estimation, [120, 130, 170, 180])
+highway_drive = mf.trapmf(x_estimation, [170, 180, 200, 230])
+# Range output membership function
+range_low = mf.trapmf(x_range, [90, 100, 135, 160])
+range_medium = mf.trapmf(x_range, [380, 390, 470, 480])
+range_high = mf.trapmf(x_range, [470, 480, 630, 640])
 
 # Plots for better overview
-fig, (ax0, ax1, ax2, ax3, ax4) = plt.subplots(nrows=5)
-ax0.plot(x_range, range_long_min, 'r', linewidth=2, label="range1")
-ax0.plot(x_range, range_long_med, 'g', linewidth=2, label="range1")
-ax0.plot(x_range, range_long_med_high, 'b', linewidth=2, label="range1")
-ax0.plot(x_range, range_long_max, 'y', linewidth=2, label="range1")
+fig, (ax0, ax1, ax2, ax3) = plt.subplots(nrows=4)
+ax0.plot(x_temperature, weather_cold, 'r', linewidth=1, label="range1")
+ax0.plot(x_temperature, weather_mild, 'g', linewidth=2, label="range1")
 
-ax1.plot(x_range, range_city, 'r', linewidth=2, label="range1")
-ax1.plot(x_range, range_highway, 'g', linewidth=2, label="range1")
+ax1.plot(x_battery, batt_low, 'r', linewidth=1, label="range1")
+ax1.plot(x_battery, batt_medium, 'g', linewidth=2, label="range1")
+ax1.plot(x_battery, batt_medium_high, 'c', linewidth=3, label="range1")
+ax1.plot(x_battery, batt_high, 'b', linewidth=4, label="range1")
 
-ax2.plot(x_range, range_temp_lower_than_min10, 'c', linewidth=4, label="range_temp")
-ax2.plot(x_range, range_temp_min10_0, 'm', linewidth=4, label="range_temp")
-ax2.plot(x_range, range_temp_0_10, 'y', linewidth=4, label="range_temp")
+ax2.plot(x_estimation, city_drive, 'c', linewidth=4, label="range_temp")
+ax2.plot(x_estimation, combined_drive, 'm', linewidth=4, label="range_temp")
+ax2.plot(x_estimation, highway_drive, 'y', linewidth=4, label="range_temp")
 
-ax3.plot(x_range, range_passengers_two, 'y', linewidth=4, label="range_pass_2")
-ax3.plot(x_range, range_passengers_four, 'y', linewidth=4, label="range_pass_4")
-ax4.plot(x_range, range_low, 'r', linewidth=4)
-ax4.plot(x_range, range_mid, 'r', linewidth=4)
-ax4.plot(x_range, range_long, 'r', linewidth=4)
+ax3.plot(x_range, range_low, 'r', linewidth=1, label="range1")
+ax3.plot(x_range, range_medium, 'c', linewidth=1, label="range1")
+ax3.plot(x_range, range_high, 'b', linewidth=1, label="range1")
+
 plt.show()
 
-# Building up interpolated membership functions, returns membership function value at input_passengers
-range_pass_two = fuzz.interp_membership(x_range, range_passengers_two, input_passengers)
-range_pass_four = fuzz.interp_membership(x_range, range_passengers_four, input_passengers)
-# returns membership function value at input_temperature
-range_temp_low = fuzz.interp_membership(x_range, range_temp_lower_than_min10, input_temperature)
-range_temp_med = fuzz.interp_membership(x_range, range_temp_min10_0, input_temperature)
-range_temp_high = fuzz.interp_membership(x_range, range_temp_0_10, input_temperature)
-# returns membership function value at input_battery_capacity
-range_batt_small = fuzz.interp_membership(x_range, range_long_min, input_battery_capacity)
-range_batt_med = fuzz.interp_membership(x_range, range_long_med, input_battery_capacity)
-range_batt_med_high = fuzz.interp_membership(x_range, range_long_med_high, input_battery_capacity)
-range_batt_high = fuzz.interp_membership(x_range, range_long_max, input_battery_capacity)
+# Building up interpolated membership functions, returns membership function value at input_temperature
+temperature_low = fuzz.interp_membership(x_temperature, weather_cold, input_temperature)
+temperature_high = fuzz.interp_membership(x_temperature, weather_mild, input_temperature)
+# Building up interpolated membership functions, returns membership function value at input_battery_capacity
+batt_low = fuzz.interp_membership(x_battery, batt_low, input_battery_capacity)
+batt_medium = fuzz.interp_membership(x_battery, batt_medium, input_battery_capacity)
+batt_medium_high = fuzz.interp_membership(x_battery, batt_medium_high, input_battery_capacity)
+batt_high = fuzz.interp_membership(x_battery, batt_high, input_battery_capacity)
+# Building up interpolated membership functions, returns membership function value at driving_style
+city_drive = fuzz.interp_membership(x_estimation, city_drive, driving_style)
+combined_drive = fuzz.interp_membership(x_estimation, combined_drive, driving_style)
+highway_drive = fuzz.interp_membership(x_estimation, highway_drive, driving_style)
 
 # Creating fuzzy rules
 # Fuzzy rules are a collection of linguistic statements that describe how the FIS (Fuzzy Inference System)
@@ -87,40 +78,47 @@ range_batt_high = fuzz.interp_membership(x_range, range_long_max, input_battery_
 # is called fuzzification. Also, we must define what we mean by "and" / "or" in the
 # fuzzy rule. This is called fuzzy combination.
 
-rule1 = np.fmin(np.fmin(np.fmin(np.fmin(range_pass_four, range_temp_low), range_batt_small), range_city), range_low)
-rule2 = np.fmin(np.fmin(np.fmin(np.fmin(range_pass_two, range_temp_low), range_batt_small), range_city), range_low)
-rule3 = np.fmin(np.fmin(np.fmin(np.fmin(range_pass_two, range_temp_med), range_batt_med), range_city), range_low)
-rule4 = np.fmin(np.fmin(np.fmin(np.fmin(range_pass_two, range_temp_med), range_batt_med_high), range_city), range_low)
-rule5 = np.fmin(np.fmin(np.fmin(np.fmin(range_pass_two, range_temp_high), range_batt_med_high), range_city), range_low)
-rule6 = np.fmin(np.fmin(np.fmin(np.fmin(range_pass_two, range_temp_high), range_batt_high), range_city), range_low)
-rule7 = np.fmin(np.fmin(np.fmin(np.fmin(range_pass_two, range_temp_min10_0), range_batt_small), range_city), range_low)
-rule8 = np.fmin(np.fmin(np.fmin(np.fmin(range_pass_two, range_temp_lower_than_min10), range_batt_small), range_city), range_low)
+rule1 = np.fmin(np.fmin(np.fmin(batt_low, weather_cold), city_drive), range_high)
+rule2 = np.fmin(np.fmin(np.fmin(batt_low, weather_cold), highway_drive), range_low)
+rule3 = np.fmin(np.fmin(np.fmin(batt_low, weather_cold), combined_drive), range_medium)
 
-rule9 = np.fmin(np.fmin(np.fmin(np.fmin(range_pass_four, range_temp_low), range_batt_small), range_city), range_mid)
-rule10 = np.fmin(np.fmin(np.fmin(np.fmin(range_pass_two, range_temp_low), range_batt_small), range_city), range_mid)
-rule11 = np.fmin(np.fmin(np.fmin(np.fmin(range_pass_two, range_temp_med), range_batt_med), range_city), range_mid)
-rule12 = np.fmin(np.fmin(np.fmin(np.fmin(range_pass_two, range_temp_med), range_batt_med_high), range_city), range_mid)
-rule13 = np.fmin(np.fmin(np.fmin(np.fmin(range_pass_two, range_temp_high), range_batt_med_high), range_city), range_mid)
-rule14 = np.fmin(np.fmin(np.fmin(np.fmin(range_pass_two, range_temp_high), range_batt_high), range_city), range_mid)
-rule15 = np.fmin(np.fmin(np.fmin(np.fmin(range_pass_two, range_temp_min10_0), range_batt_small), range_city), range_mid)
-rule16 = np.fmin(np.fmin(np.fmin(np.fmin(range_pass_two, range_temp_lower_than_min10), range_batt_small), range_city), range_mid)
+rule4 = np.fmin(np.fmin(np.fmin(batt_low, weather_mild), city_drive), range_high)
+rule5 = np.fmin(np.fmin(np.fmin(batt_low, weather_mild), highway_drive), range_low)
+rule6 = np.fmin(np.fmin(np.fmin(batt_low, weather_mild), combined_drive), range_medium)
 
-rule17 = np.fmin(np.fmin(np.fmin(np.fmin(range_pass_four, range_temp_low), range_batt_small), range_city), range_long)
-rule18 = np.fmin(np.fmin(np.fmin(np.fmin(range_pass_two, range_temp_low), range_batt_small), range_city), range_long)
-rule19 = np.fmin(np.fmin(np.fmin(np.fmin(range_pass_two, range_temp_med), range_batt_med), range_city), range_long)
-rule20 = np.fmin(np.fmin(np.fmin(np.fmin(range_pass_two, range_temp_med), range_batt_med_high), range_city), range_long)
-rule21 = np.fmin(np.fmin(np.fmin(np.fmin(range_pass_two, range_temp_high), range_batt_med_high), range_city), range_long)
-rule22 = np.fmin(np.fmin(np.fmin(np.fmin(range_pass_two, range_temp_high), range_batt_high), range_city), range_long)
-rule23 = np.fmin(np.fmin(np.fmin(np.fmin(range_pass_two, range_temp_min10_0), range_batt_small), range_city), range_long)
-rule24 = np.fmin(np.fmin(np.fmin(np.fmin(range_pass_two, range_temp_lower_than_min10), range_batt_small), range_city), range_long)
+rule7 = np.fmin(np.fmin(np.fmin(batt_medium, weather_cold), city_drive), range_high)
+rule8 = np.fmin(np.fmin(np.fmin(batt_medium, weather_cold), highway_drive), range_low)
+rule9 = np.fmin(np.fmin(np.fmin(batt_medium, weather_cold), combined_drive), range_medium)
 
-range_low = np.fmax(np.fmax(np.fmax(np.fmax(np.fmax(np.fmax(np.fmax(rule1, rule2), rule3), rule4), rule5), rule6), rule7), rule8)
-range_mid = np.fmax(np.fmax(np.fmax(np.fmax(np.fmax(np.fmax(np.fmax(rule9, rule10), rule11), rule12), rule13), rule14), rule15), rule16)
-range_long = np.fmax(np.fmax(np.fmax(np.fmax(np.fmax(np.fmax(np.fmax(rule17, rule18), rule19), rule20), rule21), rule22), rule23), rule24)
+rule10 = np.fmin(np.fmin(np.fmin(batt_medium, weather_mild), city_drive), range_high)
+rule11 = np.fmin(np.fmin(np.fmin(batt_medium, weather_mild), highway_drive), range_low)
+rule12 = np.fmin(np.fmin(np.fmin(batt_medium, weather_mild), combined_drive), range_medium)
 
-range = np.fmax(np.fmax(range_low, range_mid), range_long)
+rule13 = np.fmin(np.fmin(np.fmin(batt_medium, weather_cold), city_drive), range_high)
+rule14 = np.fmin(np.fmin(np.fmin(batt_medium, weather_cold), highway_drive), range_low)
+rule15 = np.fmin(np.fmin(np.fmin(batt_medium, weather_cold), combined_drive), range_medium)
 
-defuzzified = fuzz.defuzz(x_range, range, "mom")
+rule16 = np.fmin(np.fmin(np.fmin(batt_medium, weather_mild), city_drive), range_high)
+rule17 = np.fmin(np.fmin(np.fmin(batt_medium, weather_mild), highway_drive), range_low)
+rule18 = np.fmin(np.fmin(np.fmin(batt_medium, weather_mild), combined_drive), range_medium)
+
+rule19 = np.fmin(np.fmin(np.fmin(batt_high, weather_cold), city_drive), range_high)
+rule20 = np.fmin(np.fmin(np.fmin(batt_high, weather_cold), highway_drive), range_low)
+rule21 = np.fmin(np.fmin(np.fmin(batt_high, weather_cold), combined_drive), range_medium)
+
+rule22 = np.fmin(np.fmin(np.fmin(batt_high, weather_mild), city_drive), range_high)
+rule23 = np.fmin(np.fmin(np.fmin(batt_high, weather_mild), highway_drive), range_low)
+rule24 = np.fmin(np.fmin(np.fmin(batt_high, weather_mild), combined_drive), range_medium)
+
+#Mamdani inference system
+
+range_low_ = np.fmax(np.fmax(np.fmax(np.fmax(np.fmax(np.fmax(np.fmax(rule2, rule5), rule8), rule11), rule14), rule17), rule20), rule23)
+range_medium_ = np.fmax(np.fmax(np.fmax(np.fmax(np.fmax(np.fmax(np.fmax(rule3, rule6), rule9), rule12), rule15), rule18), rule21), rule24)
+range_high_ = np.fmax(np.fmax(np.fmax(np.fmax(np.fmax(np.fmax(np.fmax(rule1, rule4), rule7), rule10), rule13), rule16), rule19), rule22)
+
+range_ = np.fmax(np.fmax(range_low_, range_medium_), range_high_)
+
+defuzzified = fuzz.defuzz(x_range, range_, "mom")
 
 
 print("Range is:", defuzzified)
